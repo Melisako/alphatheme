b98af9fb70afb1c9168f20a219576b42414b819f
Laurat <lauratzeqiri@hotmail.com>
2023-06-20 22:35:10 +0200

    alpha

--- header.php
+++ header.php
@@ -3,11 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a1c656b99c.js" crossorigin="anonymous"></script>
     <title>Document</title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class() ?> >
+<section class="header">
+<div class="container">
+<div class="img">
+<img src="<?php echo get_stylesheet_directory_uri() . '/img/foto1.svg'; ?>" alt="Foto 1" class="left-img">
+ <img src="<?php echo get_stylesheet_directory_uri() . '/img/vector.svg'; ?>" alt="Foto 2" class="right-img">
+</div>
+    <div class="navmenu">
+ <?php   wp_nav_menu(array('theme_location' => 'primary')) ;?>
 
-<?php wp_nav_menu(array('theme_location' => 'primary'));
-?>
-    
+<ul>
+</div>
+</div>
+
+</section>
