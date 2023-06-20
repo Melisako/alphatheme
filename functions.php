a69187540acb50f939a877aaad2f4b343d9634da
Laurat <lauratzeqiri@hotmail.com>
2023-06-20 10:22:54 +0200

    alpha

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+function alpha_script() {
+    wp_enqueue_style( 'customestyle', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all' );
+}
+add_action( 'wp_enqueue_scripts', 'alpha_script' );
+
+function alpha_theme_setup(){
+    add_theme_support( 'menus');
+
+    register_nav_menu( 'primary', 'Primary Nav Menu'); 
+    register_nav_menu( 'secondary', 'Secondary Nav Menu');
+
+}
+add_action( 'init', 'alpha_theme_setup');
+?>
