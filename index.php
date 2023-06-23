aa9682cadb344c2c485dea212f4d51fbaae24921
Melisako <120452771+Melisako@users.noreply.github.com>
2023-06-23 09:26:50 +0200

    main-commit

--- index.php
+++ index.php
@@ -1,79 +1,137 @@
-
 <?php get_header();?>
 
-<?php if (have_rows('content')) : ?>
-    <?php while (have_rows('content')) : the_row(); ?>
-        <?php if (get_row_layout() == 'columns_section'): 
+
+
+
+
+
+
+
+<h1>Pros & Cons Module</h1>
+
+<div class="container">
+  <div class="pros">
+    <h2>PROS</h2>
+
+    <?php if (have_rows('module')) : ?>
+    <?php while (have_rows('module')) : the_row(); ?>
+    <?php if (get_row_layout() == 'pro_section'):
             $columns = get_sub_field('columns');
         ?>
 
-        <style>
-            body {
-                background-color: #606C5D;
-                }
-                .columns-section {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-top: 50px;
-    margin-left: 55px;
-}
+    <div class="columns-section">
+      <?php foreach($columns as $column): ?>
+      <div class="column">
+   
+        <p><?php echo $column['content']; ?></p>
+        <i class="fas fa-thumbs-up"></i>
+      </div>
+      <?php endforeach; ?>
+    </div>
 
-.column {
-    padding: 20px;
-    background-color: #f7e6c7;
-                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
-                border: 1px solid #e4cda2;
-                border-radius: 8px;
-                font-family: 'Courier New', monospace;
-    width: 48%; /* Adjust the width as needed */
-    margin-bottom: 20px; /* Add margin between columns */
-}
-
-           
-
-            .column h3 {
-                margin-top: 0;
-                font-size: 20px;
-                font-weight: bold;
-                color: #bf744b;
-                text-transform: uppercase;
-            }
-
-            .column p {
-                margin-bottom: 0;
-                font-size: 16px;
-                line-height: 1.2;
-                color: #5e4635;
-            }
-            h2 {margin-top: 40px;
-                color:  #f7e6c7;
-               display: flex;
-               flex-direction: row;
-            align-items: center;
-               align-content: center;
-                margin-left: 290px;
-               font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-               
-            }
-        </style>
- <h2>PROS <i class="fas fa-thumbs-up" style="padding-left: 10px;"></i></h2>
-
-        <div class="columns-section">
-       
-            <?php foreach($columns as $column): ?>
-                <div class="column">
-                    <h3><?php echo $column['title']; ?></h3>
-                    <p><?php echo $column['content']; ?></p>
-                </div>
-            <?php endforeach; ?>
-        </div>
-
-        <?php endif; ?>
+    <?php endif; ?>
     <?php endwhile; ?>
-<?php endif; ?>
+    <?php endif; ?>
+  </div>
 
+  <div class="cons">
+    <h2>CONS</h2>
 
+    <?php if (have_rows('cons')) : ?>
+    <?php while (have_rows('cons')) : the_row(); ?>
+    <?php if (get_row_layout() == 'cons_module'):
+            $columns = get_sub_field('column');
+        ?>
 
+    <div class="columns-section">
+      <?php foreach($columns as $column): ?>
+      <div class="column">
+       
+        <p><?php echo $column['content']; ?></p>
+        <i class="fas fa-thumbs-down"></i>
+      </div>
+      <?php endforeach; ?>
+    </div>
 
+    <?php endif; ?>
+    <?php endwhile; ?>
+    <?php endif; ?>
+  </div>
+</div>
 
+<style>
+      .fas.fa-thumbs-up {
+    color: green;
+    font-size: 24px;
+    margin-right: 5px;
+    margin-left: 350px;
+  }
+  .fas.fa-thumbs-down {
+    color: red;
+    font-size: 24px;
+    margin-right: 5px;
+    margin-left: 350px;
+  }
+
+  .container {
+    display: flex;
+  }
+
+  h1 {
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    margin-left: 120px;
+    margin-top: 40px;
+  }
+
+  .pros,
+  .cons {
+    margin-top: 40px;
+    flex: 1;
+    padding: 10px;
+    box-sizing: border-box;
+  }
+
+  .column {
+    display: flex;
+    align-items: center;
+  }
+
+  .column p {
+    padding-left: 20px;
+    font-size: 16px;
+  }
+  
+  .columns-section {
+  
+    margin-left: 35px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .cons .column {
+    width: 500px;
+    height: 43px;
+    margin-bottom: 10px;
+    border-radius: 15px;
+    background: linear-gradient(to right, #FF0000 0%, #FF0000 5px, rgba(255, 0, 0, 0.1) 5px, rgba(255, 0, 0, 0.1) 100%);
+  }
+
+  .pros .column {
+    width: 500px;
+    height: 43px;
+    margin-bottom: 10px;
+    border-radius: 15px;
+    background: linear-gradient(to right, #008000 0%, #008000 5px, rgba(0, 128, 0, 0.1) 5px, rgba(0, 128, 0, 0.1) 100%);
+  }
+  
+  h2 {
+    margin-left: 38px;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    margin-bottom: 10px;
+    font-weight: bold;
+    font-size: 20px;
+  }
+</style>
+
+<!-- Include the Font Awesome library -->
+<script src="your-font-awesome-kit.js" crossorigin="anonymous"></script>
