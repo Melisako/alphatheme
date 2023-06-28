1178f0f0c1c0fe0977eb9a2722857f26535b61ba
Melisako <120452771+Melisako@users.noreply.github.com>
2023-06-28 23:02:11 +0200

    module-commit

--- header.php
+++ header.php
@@ -6,18 +6,32 @@
     <script src="https://kit.fontawesome.com/a1c656b99c.js" crossorigin="anonymous"></script>
     <title>Document</title>
     <?php wp_head(); ?>
+    <style>
+        /* Add your custom CSS styles here */
+    </style>
 </head>
-<body <?php body_class() ?> >
-<section class="header">
-<div class="container">
-<div class="img">
-<img src="<?php echo get_stylesheet_directory_uri() . '/img/foto1.svg'; ?>"  class="left">
- <img src="<?php echo get_stylesheet_directory_uri() . '/img/vector.svg'; ?>"  class="right">
-</div>
-    <div class="navmenu">
- <?php   wp_nav_menu(array('theme_location' => 'primary')) ;?>
-<ul>
-</div>
-</div>
+<body <?php body_class(); ?>>
 
+<section class="header">
+    <div class="container">
+        <div class="img">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/img/foto1.svg'; ?>" class="left">
+            <img src="<?php echo get_stylesheet_directory_uri() . '/img/vector.svg'; ?>" class="right">
+        </div>
+        <div class="navmenu">
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'menu_class' => 'menu',
+                'container' => 'ul',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s<li><a href="' . get_template_directory_uri() . '/modules/prosCons.php">Pros & Cons</a></li></ul>'
+            ));
+            ?>
+        </div>
+    </div>
 </section>
+
+<!-- Rest of your code -->
+
+</body>
+</html>
--- modules/prosCons.php
+++ modules/prosCons.php
@@ -1,4 +1,6 @@
 
+<!-- Include the header -->
+<?php get_header(); ?>
 
 
 <h1>Pros & Cons Module</h1>
@@ -27,79 +29,7 @@
     <?php endwhile; ?>
     <?php endif; ?>
   </div>
-  <style>
- .fas.fa-thumbs-up {
-    color: green;
-    font-size: 24px;
-    margin-right: 5px;
-    margin-left: 350px;
-  }
-  .fas.fa-thumbs-down {
-    color: red;
-    font-size: 24px;
-    margin-right: 5px;
-    margin-left: 350px;
-  }
 
-  .container {
-    display: flex;
-  }
-
-  h1 {
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    margin-left: 120px;
-    margin-top: 40px;
-  }
-
-  .pros,
-  .cons {
-    margin-top: 40px;
-    flex: 1;
-    padding: 10px;
-    box-sizing: border-box;
-  }
-
-  .column {
-    display: flex;
-    align-items: center;
-  }
-
-  .column p {
-    padding-left: 20px;
-    font-size: 16px;
-  }
-  
-  .columns-section {
-  
-    margin-left: 35px;
-    display: flex;
-    flex-direction: column;
-  }
-
-  .cons .column {
-    width: 500px;
-    height: 43px;
-    margin-bottom: 10px;
-    border-radius: 15px;
-    background: linear-gradient(to right, #FF0000 0%, #FF0000 5px, rgba(255, 0, 0, 0.1) 5px, rgba(255, 0, 0, 0.1) 100%);
-  }
-
-  .pros .column {
-    width: 500px;
-    height: 43px;
-    margin-bottom: 10px;
-    border-radius: 15px;
-    background: linear-gradient(to right, #008000 0%, #008000 5px, rgba(0, 128, 0, 0.1) 5px, rgba(0, 128, 0, 0.1) 100%);
-  }
-  
-  h2 {
-    margin-left: 38px;
-    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    margin-bottom: 10px;
-    font-weight: bold;
-    font-size: 20px;
-  }
-    </style>
 
   <div class="cons">
     <h2>CONS</h2>
@@ -126,7 +56,7 @@
   </div>
 </div>
 
-
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri() . '/css/style.css'; ?>">
 
 <!-- Include the Font Awesome library -->
 <script src="your-font-awesome-kit.js" crossorigin="anonymous"></script>
