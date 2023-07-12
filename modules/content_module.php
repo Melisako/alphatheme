e3b723e166f9ba65189d229fe59a32bf946be6a8
LauratZeqiri <128151479+LauratZeqiri@users.noreply.github.com>
2023-07-12 11:52:54 +0200

    alpha

--- modules/content_module.php
+++ modules/content_module.php
@@ -1,35 +1,18 @@
-<?php 
-    $modules = [];
-    $fields = [];
-    if(have_rows('content_module')):
-        while( have_rows('content_module') ) : the_row();
-            $modules[] = get_sub_field('content_module');
-            $fields[] = get_sub_field('content_field');
-        endwhile;
-    endif;
+<?php
+
+if (get_row_layout() === 'content_module') {
+
+    $textarea = get_sub_field('textarea');
+  
+}
 ?>
+<div class="content-section">
 <div class="container">
-<div class="content">
-    <div class="content_modules">
-        <div class="content__modules__title">Content Module:</div>
-        <div class="content__modules__items">
-            <?php foreach($modules as $module) { ?>
-                <div class="content__modules__items__item">
-                    <?= $module; ?>
-                </div>
-            <?php } ?>
-        </div>
-    </div>
-    <div class="content__fields">
-        <div class="content__fields__title">Content Field:</div>
-            <div class="content__fields__items">
-                <?php foreach($fields as $field) { ?>
-                    <div class="content__fields__items__item">
-                        <?= $field; ?>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
+    <div class="content-module">
+<p class="content_module_text"><?php echo $textarea; ?></p>
 </div>
-               
+</div>
+</div>
+
+
+ 
