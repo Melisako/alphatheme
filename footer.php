6394f32db05c8c307276a0c23ed6b9e70181db9d
Shqipdon22 <121349621+Shqipdon22@users.noreply.github.com>
2023-07-12 15:55:32 +0200

    scroll to top button finish

--- footer.php
+++ footer.php
@@ -1,45 +1,65 @@
 <?php include(__DIR__ . '/sections/author.php'); ?>
 <footer>
+        <button onclick="topFunction()" id="myBtn" title="Go to top" class="fa fa-angle-up"></button>
+        <script>
+                let mybutton = document.getElementById("myBtn");
 
-<div class="container">
-        <div class="footer-section">
-<div class="left-footer">
+               
+                window.onscroll = function() {
+                        scrollFunction()
+                };
 
-<img src="<?php echo get_stylesheet_directory_uri() . '/img/icon.svg'; ?>"  class="left">
+                function scrollFunction() {
+                        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                                mybutton.style.display = "block";
+                        } else {
+                                mybutton.style.display = "none";
+                        }
+                }
 
-<div class="text">
-        <h3>About Alpha Theme </h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum tenetur voluptatem, alias animi magni ab, repudiandae, 
-nihil voluptatibus voluptatum aperiam accusantium. Mollitia ab esse reprehenderit ullam, sunt fugiat odit nostrum? </p>
-</div>
-<div class="social">
-<span> Follow Us </span>
-<div class="logo">
-        <a href="#"><i class="fa-brands fa-facebook"></i></a>
-        <a href="#"> <i class="fa-brands fa-instagram"></i></a>
-        <a href="#"><i class="fa-brands fa-facebook"></i></a>
-        <a href="#"> <i class="fa-brands fa-instagram"></i></a>
-        <a href="#"><i class="fa-brands fa-facebook"></i></a>
-        <a href="#"> <i class="fa-brands fa-instagram"></i></a>
+                function topFunction() {
+                        document.body.scrollTop = 0; 
+                        document.documentElement.scrollTop = 0; 
+                }
+        </script>
+        <div class="container">
+                <div class="footer-section">
+                        <div class="left-footer">
+
+                                <img src="<?php echo get_stylesheet_directory_uri() . '/img/icon.svg'; ?>" class="left">
+
+                                <div class="text">
+                                        <h3>About Alpha Theme </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum tenetur voluptatem, alias animi magni ab, repudiandae,
+                                                nihil voluptatibus voluptatum aperiam accusantium. Mollitia ab esse reprehenderit ullam, sunt fugiat odit nostrum? </p>
+                                </div>
+                                <div class="social">
+                                        <span> Follow Us </span>
+                                        <div class="logo">
+                                                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                                <a href="#"> <i class="fa-brands fa-instagram"></i></a>
+                                                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                                <a href="#"> <i class="fa-brands fa-instagram"></i></a>
+                                                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                                <a href="#"> <i class="fa-brands fa-instagram"></i></a>
 
 
-       
-</div>
-</div>
-</div>
-<div class="right-footer">
-<div class="menu1">
-        <h3>Site Links </h3>
 
-<?php  wp_nav_menu(array('theme_location' => 'secondary'));?>
-</div>
-<div class="menu2">
-<h3>Our Sites</h3>
-<?php wp_nav_menu(array('theme_location' => 'third'));?>
-</div>
-</div>
-</div>
-</div>
-<p class="footer-paragraf">2023. All rights reserved. </p>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="right-footer">
+                                <div class="menu1">
+                                        <h3>Site Links </h3>
+
+                                        <?php wp_nav_menu(array('theme_location' => 'secondary')); ?>
+                                </div>
+                                <div class="menu2">
+                                        <h3>Our Sites</h3>
+                                        <?php wp_nav_menu(array('theme_location' => 'third')); ?>
+                                </div>
+                        </div>
+                </div>
+        </div>
+        <p class="footer-paragraf">2023. All rights reserved. </p>
 </footer>
-
